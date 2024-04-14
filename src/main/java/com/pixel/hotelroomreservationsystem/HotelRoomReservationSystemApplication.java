95b50fa41f664c0aa8c7402d5b4bb41811b09f54
Thiraj Pramodya <thiraj.pro@gmail.com>
2024-04-14 14:01:33 +0530

    Initial commit

---
+++ src/main/java/com/pixel/hotelroomreservationsystem/HotelRoomReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pixel.hotelroomreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelRoomReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixel/hotelroomreservationsystem/HotelRoomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixel.hotelroomreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
